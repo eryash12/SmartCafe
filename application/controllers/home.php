5d0f9da10781a3c8fc190292881052e193cc7e56
eryash12 <yash.tamakuwala@gmail.com>
2016-02-24 22:40:19 -0800

    mqtt

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,9 +8,55 @@
 class home extends CI_Controller{
     function index()
     {
-        echo "<pre>";
-        print_r("yash");
-        echo "</pre>";
+
+        $params["host"] = "m10.cloudmqtt.com";
+        $params["port"] = 11740;
+        $username = "pcrfytjr";
+        $password = "6PYy_bKDFUyt";
+
+        $params["client_id"] = "ClientID".rand();
+        $this->load->library('phpMQTT',$params);
+
+        if(!$this->phpmqtt->connect(true,NULL,"pcrfytjr","6PYy_bKDFUyt")){
+            echo "yes";
+            exit(1);
+        }
+
+        //currently subscribed topics
+        $topics['outTopic'] = array("qos"=>0, "function"=>"procmsg");
+
+        $this->phpmqtt->subscribe($topics,0);
+        exit;
+        while($this->phpmqtt->proc()){
+
+        }
+
+        $this->phpmqtt->close();
+        function procmsg($topic,$msg){
+            echo "Msg Recieved: $msg";
+        }
+
+
     }
+    function publish(){
+
+        $params["host"] = "m10.cloudmqtt.com";
+        $params["port"] = 11740;
+        $username = "pcrfytjr";
+        $password = "6PYy_bKDFUyt";
+        $message = "publish succesfull";
+
+        $params["client_id"] = "ClientID".rand();
+        $this->load->library('phpMQTT',$params);
+
+        if ($this->phpmqtt->connect(true,NULL,"pcrfytjr","6PYy_bKDFUyt")) {
+            $this->phpmqtt->publish("topic",$message, 0);
+            $this->phpmqtt->close();
+        }else{
+            echo "Fail or time out<br />";
+        }
+    }
+
+
 }
 ?>
