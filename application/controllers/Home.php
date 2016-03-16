4f9f135673f72ebc9a09ef7c08065a7aaddfb889
eryash12 <yash.tamakuwala@gmail.com>
2016-03-16 12:17:27 -0700

    current_value

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -34,14 +34,14 @@ class Home extends CI_Controller
     }
 
     function send_data($temp, $irr, $eff, $power)
-    {
+    {   $this->load->model('user_model');
         $system = $this->user_model->get_current_value('system');
         $valve = $this->user_model->get_current_value('valve');
         $threshold = $this->user_model->get_current_value('threshold');
         $system = $system[0]["value"];
         $valve = $valve[0]["value"];
         $threshold = $threshold[0]["value"];
-        $this->load->model('user_model');
+
         if($system == "1")
         $this->user_model->write_data($temp, $irr, $eff, $power);
 //        $system = $this->user_model->get_current_value('system');
